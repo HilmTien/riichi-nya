16426e94f0a0eaea0ec5cf1f03038da0d3fd8ecc
Tien Tran <tient2477@gmail.com>
2026-07-07 02:35:58 +0200

    feat: make server interface

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,8 @@
 import { serve } from "bun";
 import { parseClientMessage } from "./lib/utils";
+import { Timer } from "./timer";
+
+const timer = new Timer(3, 10, 3);
 
 const server = serve({
   port: 8000,
@@ -12,17 +15,102 @@ const server = serve({
   },
   websocket: {
     message(ws, message) {
-      const messageString = typeof message === "string" ? message : new TextDecoder().decode(message);
+      const messageString =
+        typeof message === "string"
+          ? message
+          : new TextDecoder().decode(message);
       const clientMessage = parseClientMessage(messageString);
       if (!clientMessage) {
         ws.send("Invalid message");
         return;
       }
 
-      switch (clientMessage.type) {
-        case "ping":
-          ws.send(JSON.stringify({ type: "pong", timestamp: Date.now() }));
-          break;
+      // const onTimeout = () => {
+      //   ws.send(
+      //     JSON.stringify({
+      //       type: "set_turn",
+      //       currentTurn: timer.getCurrentTurn(),
+      //     })
+      //   );
+      // };
+
+      const sendState = () => {
+        ws.send(
+          JSON.stringify({
+            type: "state",
+            currentTurn: timer.getCurrentTurn(),
+            extraTimers: timer.getExtraTimers(),
+            hasStarted: timer.getHasStarted(),
+            state: timer.getState(),
+          })
+        );
+      };
+
+      const onTimeout = sendState;
+
+      try {
+        switch (clientMessage.type) {
+          case "ping":
+            ws.send(JSON.stringify({ type: "pong", timestamp: Date.now() }));
+            break;
+          case "state":
+            sendState();
+            break;
+          case "join":
+            ws.send(
+              JSON.stringify({
+                type: "set_player",
+                player: clientMessage.player,
+              })
+            );
+            break;
+          case "start":
+            timer.start(onTimeout);
+            sendState();
+            break;
+          case "reset":
+            timer.reset();
+            sendState();
+            break;
+          case "pon":
+            timer.call(clientMessage.caller, "pon", onTimeout);
+            sendState();
+            break;
+          case "chii":
+            timer.call(clientMessage.caller, "chii", onTimeout);
+            sendState();
+            break;
+          case "kan":
+            timer.call(clientMessage.caller, "kan", onTimeout);
+            sendState();
+            break;
+          case "ron":
+            timer.ron(clientMessage.caller);
+            sendState();
+            break;
+          case "skip":
+            timer.voteSkip(clientMessage.caller);
+            sendState();
+            break;
+          case "riichi":
+            timer.riichi(clientMessage.caller, onTimeout);
+            sendState();
+            break;
+          case "tsumo":
+            timer.tsumo(clientMessage.caller);
+            sendState();
+            break;
+          case "discard":
+            timer.discard(onTimeout);
+            sendState();
+            break;
+        }
+      } catch (e) {
+        if (e instanceof Error) {
+          ws.send(e.message);
+        } else {
+          throw e;
+        }
       }
     }, // a message is received
     open(ws) {}, // a socket is opened
--- server/src/types/websocket.ts
+++ server/src/types/websocket.ts
@@ -1,7 +1,21 @@
 import z from "zod";
 
+const player = ["E", "S", "W", "N"] as const;
+
 export const ClientMessageSchema = z.discriminatedUnion("type", [
-  z.object({ type: z.literal("ping"), timestamp: z.number() }),
+  z.object({ type: z.literal("ping") }),
+  z.object({ type: z.literal("state") }),
+  z.object({ type: z.literal("start") }),
+  z.object({ type: z.literal("reset") }),
+  z.object({ type: z.literal("join"), player: z.enum(player) }),
+  z.object({ type: z.literal("pon"), caller: z.enum(player) }),
+  z.object({ type: z.literal("chii"), caller: z.enum(player) }),
+  z.object({ type: z.literal("kan"), caller: z.enum(player) }),
+  z.object({ type: z.literal("ron"), caller: z.enum(player) }),
+  z.object({ type: z.literal("skip"), caller: z.enum(player) }),
+  z.object({ type: z.literal("tsumo"), caller: z.enum(player) }),
+  z.object({ type: z.literal("riichi"), caller: z.enum(player) }),
+  z.object({ type: z.literal("discard"), caller: z.enum(player) }),
 ]);
 
 export type ClientMessage = z.infer<typeof ClientMessageSchema>;
